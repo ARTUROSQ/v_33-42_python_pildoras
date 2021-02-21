abb569429f5fa55d41fdc8c15158f9d343cfe2e9
Arturo <lin.arturox@gmail.com>
2021-02-21 00:24:31 -0600

    Se crearon modulos y se importaron sentencias from, import y el comodin *

---
+++ Modulos/uso_funciones.py
@@ -0,0 +1,16 @@
+import funciones_matematicas
+
+from funciones_matematicas import *
+
+#funciones_matematicas.sumar(7,5)
+
+#funciones_matematicas.restar(9,5)
+
+#funciones_matematicas.multiplicar(3,3)
+
+sumar(5,20)
+
+restar(20,10)
+
+multiplicar(8,8)
+
