b631a9994ca6e0bb387d158ba2be2bb4900ddf0d
Arturo <lin.arturox@gmail.com>
2021-02-27 17:36:00 -0600

    v36 se creo e instalo paquete distribuible

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+
+    name="paquetecalculos",
+    version="1.0",
+    description="Paquete de redondeo y potencia",
+    author="Arturo",
+    author_email="lin.arturox@gmail.com",
+    url="N/A",
+    packages=["Calculos", "Calculos.redondeo_potencia"]
+ 
+
+)
