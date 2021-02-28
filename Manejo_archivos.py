fa69cb1a74989ad206298c2503cc6c17f92cf790
Arturo <lin.arturox@gmail.com>
2021-02-27 23:52:36 -0600

    Se creo un archivo y de agrego contenido con io y open

---
+++ Manejo_archivos.py
@@ -0,0 +1,39 @@
+from io import open
+
+#Importamos de la libreria io el modulo open
+
+archivo_texto=open("archivo.txt", "a") #w,r,a - escritura, lectura, agregar
+
+#frase = "Estupendo dia para estudiar python \n el sabado"
+
+#archivo_texto.write(frase)
+
+#archivo_texto.close() 
+
+
+
+#texto=archivo_texto.read()
+
+#archivo_texto.close()
+
+#print(texto)
+
+
+#lineas_texto=archivo_texto.readlines()
+
+#archivo_texto.close()
+
+#print(lineas_texto[0])
+
+
+
+archivo_texto.write("\n Siempre es una buena ocasión para estudiar python")
+
+archivo_texto.close()
+
+
+
+
+
+
+
